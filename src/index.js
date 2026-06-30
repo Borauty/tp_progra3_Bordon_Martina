0d922328ddd54de96d62a6a214e68ee02142daae
MartinaMatias <matiasmartina97@gmail.com>
2026-06-30 03:59:42 -0300

    Conectar frontend con backend: detalles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 
 const PORT = environments.port;
 const app = express();
+//Middleware basico para permitir que el servidor pueda recibir y procesar peticiones con contenido en formato JSON
 app.use(cors());
 
 

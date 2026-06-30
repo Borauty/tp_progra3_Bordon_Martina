c5356408584b71cc941fbd5b6bda4424f531360a
Borauty <104461494+Borauty@users.noreply.github.com>
2026-06-29 23:37:30 -0300

    iniciando backend

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import connection from "./database/db.js" ;
+import environments from "./config/environments.js";
+
+const PORT = environments.port;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hola mundo!");
+});
+
+app.listen(3000, () => {
+    console.log(`Servidor corriendo en el puerto 3000`);
+});
+
+
+app.get("/api/products", async (req, res) =>{
+
+    try {
+        const [rows] = await connection.query("SELECT * FROM products")
+
+        res.status(200).json({
+            payload: rows
+        });
+
+    } catch (error) {
+        console.error("Error obteniendo productos: ", error.message)
+    }
+
+})
